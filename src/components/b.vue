f466d924584fcb5b7a36d62cfcc60a1a0ee91643
qingcheng <1136479388.com>
2021-06-01 15:51:19 +0800

    9

--- src/components/b.vue
+++ src/components/b.vue
@@ -1,9 +1,20 @@
 <template>
-    <div>123456</div>
+    <div>
+        <button @click="jian">-</button>
+        {{this.$store.state.num}}
+        <button @click="jia">+</button>
+    </div>
 </template>
 
 <script>
+    import {mapMutations} from "vuex"
+    export default {
+        methods:{
+            ...mapMutations(['jia','jian'])
+        }
+        
+    }
+    
 </script>
-
 <style>
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,17 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+      num:1
+  },
+  mutations: {
+      jia(state){
+          state.num++
+      },
+      jian(state){
+          state.num--
+      }  
+  },
   actions: {},
   modules: {},
 });
